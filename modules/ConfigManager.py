62a4443f42c17c68da07d907ee79ff4135cbf903
Yiang <yiangforwork@gmail.com>
2024-05-11 02:04:33 +0800

    update: config file able to save

--- modules/ConfigManager.py
+++ modules/ConfigManager.py
@@ -4,14 +4,26 @@ import toml
 class ConfigManager():
     def __init__(self) -> None:
         basepath = path.dirname(__file__)
-        filepath = path.abspath(path.join(basepath, "..", "config.toml"))
+        self.filepath = filepath = path.abspath(path.join(basepath, "..", "config.toml"))
 
-        with open( filepath, "r" ) as file:
+        with open(filepath, "r") as file:
             raw = file.read()
             self.config = toml.loads( raw )
+            file.close()
 
     def get(self, key, defaultValue):
         if key in self.config:
             return self.config[key]
         else:
             return defaultValue
+        
+    def update(self, key, val):
+        if key not in self.config: return
+        self.config[key] = val
+        self.saveConfig()
+
+    def saveConfig(self):
+        output = toml.dumps(self.config)
+        with open(self.filepath, "w") as file:
+            file.write( output )
+
