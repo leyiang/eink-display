33c29de043b0c8568530954a30dcf1e83a1d2684
Yiang <yiangforwork@gmail.com>
2024-05-11 00:04:31 +0800

    fix: hide wire not working

--- modules/WireManager.py
+++ modules/WireManager.py
@@ -11,8 +11,12 @@ class WireManager():
         self.wire = createOutlineWindow( 2*self.size.w, 2*self.size.h )
         
     def hideWire(self):
-        self.wire.destroy()
-    
+        if self.wire:
+            # self.wire.window.clear_area(0, 0, 4000, 4000)
+            self.wire.window.destroy()
+            self.wire.d.flush()
+        self.wire = None
+   
     def updateSize(self):
         self.hideWire()
         self.showWire()
